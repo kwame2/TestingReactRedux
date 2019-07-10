bf233971c8c992145c1878e8458933711db0f7ae
kwame2 <softcons99@gmail.com>
2019-07-10 17:07:27 +0200

    Head line component with test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Header from './component/header';
 import './app.scss';
+import Headline from './component/headline';
+import './app.scss';
 
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <section className="main">
+        <Headline header="Posts" desc="Please hit the button to show the details"/>
+      </section>
     </div>
   );
 }
